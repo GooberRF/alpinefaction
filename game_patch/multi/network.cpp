78a7baed60c391a854d0150bd2501de96f08eb5f
nick <nick@nickalreadyinu.se>
2026-02-22 22:36:36 -0500

    cl_footsteps

--- game_patch/multi/network.cpp
+++ game_patch/multi/network.cpp
@@ -49,6 +49,7 @@
 #include "../misc/alpine_settings.h"
 #include "../misc/waypoints.h"
 #include "../object/object.h"
+#include "../object/object_private.h"
 #include "../os/console.h"
 #include "../purefaction/pf.h"
 #include "../sound/sound.h"
@@ -1418,6 +1419,9 @@ CallHook<int(const rf::NetAddr*, std::byte*, size_t)> send_join_accept_packet_ho
         if (server_geo_chunk_physics()) {
             ext_data.flags |= AlpineFactionJoinAcceptPacketExt::Flags::geo_chunk_physics;
         }
+        if (server_allow_footsteps()) {
+            ext_data.flags |= AlpineFactionJoinAcceptPacketExt::Flags::allow_footsteps;
+        }
         auto [buf, new_len] = extend_packet_bytes(data, len, &ext_data, sizeof(ext_data));
         //auto [new_data, new_len] = extend_packet_fixed(data, len, ext_data);
         return send_join_accept_packet_hook.call_target(addr, buf.get(), new_len);
@@ -1451,6 +1455,7 @@ CodeInjection process_join_accept_injection{
             server_info.location_pinging = !!(ext_data.flags & AlpineFactionJoinAcceptPacketExt::Flags::location_pinging);
             server_info.delayed_spawns = !!(ext_data.flags & AlpineFactionJoinAcceptPacketExt::Flags::delayed_spawns);
             server_info.geo_chunk_physics = !!(ext_data.flags & AlpineFactionJoinAcceptPacketExt::Flags::geo_chunk_physics);
+            server_info.allow_footsteps = !!(ext_data.flags & AlpineFactionJoinAcceptPacketExt::Flags::allow_footsteps);
 
             constexpr float default_fov = 90.0f;
             if (!!(ext_data.flags & AlpineFactionJoinAcceptPacketExt::Flags::max_fov) && ext_data.max_fov >= default_fov) {
@@ -1460,6 +1465,9 @@ CodeInjection process_join_accept_injection{
                 server_info.semi_auto_cooldown = ext_data.semi_auto_cooldown;
             }
             g_af_server_info = std::optional{server_info};
+
+            // Update footstep activation based on server permissions
+            evaluate_footsteps();
         }
         else {
             g_af_server_info.reset();
@@ -2105,6 +2113,9 @@ FunHook<void()> multi_stop_hook{
                 static_cast<PlayerAdditionalData*>(rf::local_player);
             *player_add_data = PlayerAdditionalData{};
         }
+
+        // Re-evaluate footstep state when leaving multiplayer
+        evaluate_footsteps();
         multi_stop_hook.call_target();
 
         // Auto-quit for bots when disconnected from server
