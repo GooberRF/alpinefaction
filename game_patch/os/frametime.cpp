457196fd30562e4cba2eeea89aa16474a5e309de
Chris Parsons <gooberCP@gmail.com>
2026-04-16 14:08:21 -0230

    fix outlines and fps cap for listen server host

--- game_patch/os/frametime.cpp
+++ game_patch/os/frametime.cpp
@@ -262,7 +262,7 @@ void apply_maximum_fps()
     }
     else if (rf::is_multi) {
         const auto& server_info_opt = get_af_server_info();
-        if (server_info_opt && server_info_opt->unlimited_fps) {
+        if (rf::is_server || (server_info_opt && server_info_opt->unlimited_fps)) {
             max_fps = g_alpine_game_config.max_fps;
         }
         else {
