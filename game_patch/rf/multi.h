e584eb2aa8c5234dbe7028a69de07a1b33807899
Chris Parsons <gooberCP@gmail.com>
2026-05-25 10:41:28 -0230

    Correct return statement in hash function
    Fix hash function implementation to return correct value.

--- game_patch/rf/multi.h
+++ game_patch/rf/multi.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <format>
+#include <functional>
 #include <patch_common/MemUtils.h>
 #include "os/vtypes.h"
 #include "os/timestamp.h"
@@ -381,7 +382,7 @@ struct std::hash<rf::NetAddr> {
     size_t operator()(const rf::NetAddr& net_addr) const noexcept {
         const size_t ip_addr_hash = std::hash<uint32_t>{}(net_addr.ip_addr.inner);
         const size_t port_hash = std::hash<uint16_t>{}(net_addr.port);
-        return ip_addr_hash 
+        return ip_addr_hash
             ^ (port_hash + 0x9E3779B9 + (ip_addr_hash << 6) + (ip_addr_hash >> 2));
     }
 };
