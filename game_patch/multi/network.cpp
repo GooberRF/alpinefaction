82f83b230b71826b8c2501f7829d81a1bad6d539
Chris Parsons <gooberCP@gmail.com>
2026-07-31 08:38:11 -0230

    Clean up dead pf compat stub code (#403)
    * initial
    
    * fix

--- game_patch/multi/network.cpp
+++ game_patch/multi/network.cpp
@@ -2676,7 +2676,6 @@ CodeInjection send_state_info_injection{
     [](auto& regs) {
         rf::Player* player = regs.edi;
         trigger_send_state_info(player);
-        pf_player_level_load(player);
         sprays_force_state_sync_to(player);
     },
 };
@@ -2685,7 +2684,6 @@ FunHook<void(rf::Player*)> send_players_packet_hook{
     0x00481C70,
     [](rf::Player *player) {
         send_players_packet_hook.call_target(player);
-        pf_player_init(player);
         if (rf::is_server) {
             server_reliable_socket_ready(player);
         }
@@ -2715,7 +2713,7 @@ FunHook<void __fastcall(void*, int, int, bool, int)> multi_io_stats_add_hook{0x0
 static void process_custom_packet([[maybe_unused]] void* data, [[maybe_unused]] int len,
                                   [[maybe_unused]] const rf::NetAddr& addr, [[maybe_unused]] rf::Player* player)
 {
-    pf_process_packet(data, len, addr, player);
+    pf_process_packet(data, len, addr);
     af_process_packet(data, len, addr, player);
 }
 
