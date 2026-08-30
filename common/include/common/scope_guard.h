792c59d6d95ff49cedddc740c2ec64c22b5171cc
is-this-c <87069698+is-this-c@users.noreply.github.com>
2026-08-31 03:01:11 +1200

    Update scope_guard.h

--- common/include/common/scope_guard.h
+++ common/include/common/scope_guard.h
@@ -7,7 +7,7 @@ struct ScopeGuard {
     F _f;
     constexpr explicit ScopeGuard(F&& f)
         noexcept(std::is_nothrow_move_constructible<F>::value)
-        : _f(std::forward<F>(func)) {
+        : _f(std::forward<F>(f)) {
     }
     ~ScopeGuard() noexcept { _f(); }
     ScopeGuard(const ScopeGuard&) = delete;
