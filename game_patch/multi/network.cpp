29203744242e3e0869735c0ebeda6382fa27e83b
is-this-c <87069698+is-this-c@users.noreply.github.com>
2026-09-05 03:03:46 +1200

    Fix

--- game_patch/multi/network.cpp
+++ game_patch/multi/network.cpp
@@ -3021,11 +3021,11 @@ void send_chat_line_packet(const std::string_view msg, rf::Player* target, rf::P
     std::strncpy(packet_msg, msg.data(), len);
     packet_msg[len] = '\0';
     if (target == nullptr && rf::is_server) {
-        rf::multi_io_send_reliable_to_all(buf, packet.header.size + sizeof(packet.header), 0);
+        rf::multi_io_send_reliable_to_all(buf, packet.header.size + sizeof(packet.header), false);
         rf::console::print("Server: {}", msg);
     }
     else {
-        rf::multi_io_send_reliable(target, buf, packet.header.size + sizeof(packet.header), 0);
+        rf::multi_io_send_reliable(target, buf, packet.header.size + sizeof(packet.header), false);
     }
 }
 
@@ -3194,32 +3194,24 @@ CodeInjection send_state_info_injection{
 };
 
 CodeInjection process_state_info_req_between_levels_patch{
-    0x00481C12,
+    0x00481C03,
     [] (auto& regs) {
-        if (regs.al) {
-            rf::Player* const player = regs.ebx;
-            rf::send_state_info(player);
+        rf::Player* const player = regs.ebx;
+        rf::send_state_info(player);
 
-            // Set `NETPLAYER_STATE_WAITING` like `mp_change_level`.
-            constexpr int NETPLAYER_STATE_WAITING = 0x0;
-            player->net_data->state = NETPLAYER_STATE_WAITING;
+        // Set `NETPLAYER_STATE_WAITING` like `mp_change_level`.
+        player->net_data->state = rf::NETPLAYER_STATE_WAITING;
 
-            const RF_GamePacketHeader enter_limbo_packet =
-                {.type = RF_GPT_ENTER_LIMBO, .size = 0};
-            rf::multi_io_send_reliable(
-                player,
-                &enter_limbo_packet,
-                enter_limbo_packet.size + sizeof(RF_GamePacketHeader),
-                FALSE
-            );
+        const RF_GamePacketHeader enter_limbo_packet =
+            {.type = RF_GPT_ENTER_LIMBO, .size = 0};
+        rf::multi_io_send_reliable(
+            player,
+            &enter_limbo_packet,
+            enter_limbo_packet.size + sizeof(RF_GamePacketHeader),
+            false
+        );
 
-            regs.eip = 0x00481C4B;
-        } else {
-            // Otherwise, kick.
-            regs.eip = 0x00481C53;
-        }
-
-        regs.esp += 0x8;
+        regs.eip = 0x00481C4B;
     },
     false
 };
@@ -3811,7 +3803,7 @@ void network_init()
 
     // Print join denials.
     check_access_for_new_player_hook.install();
-    // Fix joins that complete right before limbo.
+    // Do not kick a player, if they join right before limbo.
     process_state_info_req_between_levels_patch.install();
     // Move our limbo check to `check_join_request_restrict_status`.
     AsmWriter{0x0047AE64}.nop(6);
